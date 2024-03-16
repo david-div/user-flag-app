5d10da23278ac90b3a279f436cdcac7ab7498b77
David Divilly <d.divilly@gmail.com>
2024-03-16 11:57:26 +0100

    Initial commit

---
+++ src/test/java/com/ravenpack/userflagapp/UserFlagAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravenpack.userflagapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserFlagAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
